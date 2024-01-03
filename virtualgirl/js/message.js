6981d11028f83af57a0d1db7bc279a2e605cf8cd
gh613725 <153519898+gh613725@users.noreply.github.com>
2024-01-03 12:40:05 +0800

    Update message.js

--- virtualgirl/js/message.js
+++ virtualgirl/js/message.js
@@ -131,7 +131,7 @@ initTips();
                 text = '嗨~ 快来逗我玩吧！';
             }
         }else {
-            text = '初次见面，幸会~~';
+            text = '欢迎来到<span style="color:#0099cc;">「 ' + document.title.split(' - ')[0] + ' 」</span>';
         }
     }
     showMessage(text, 12000);
